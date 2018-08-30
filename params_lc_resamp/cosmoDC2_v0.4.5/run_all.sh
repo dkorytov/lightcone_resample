40dc4a47979ca98aaff26fa956e5fe90e8055299
Dan Korytov <dkorytov@cooleylogin2.fst.alcf.anl.gov>
2018-08-30 16:12:15 +0000

    added parameter files

---
+++ params_lc_resamp/cosmoDC2_v0.4.5/run_all.sh
@@ -0,0 +1,3 @@
+mv logs/*.err logs/old/.
+mv logs/*.out logs/old/.
+qsub -n 1 -t 720 -o params_lc_resamp/cosmoDC2_v0.4.5/logs/0_1.out -e params_lc_resamp/cosmoDC2_v0.4.5/logs/0_1.err  --debuglog=params_lc_resamp/cosmoDC2_v0.4.5/logs/0_1.colbalt  ./lc_resample.py params_lc_resamp/cosmoDC2_v0.4.5/cosmoDC2_v0.4.5_z_0_1.param
---
+++ params_lc_resamp/cosmoDC2_v0.4.5/run_cosmoDC2_v0.4.5_z_0_1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=0.4.5_0_1
+#SBATCH -o params_lc_resamp/protoDC2_v0.4.5/logs/v0.4.5_z_0_1_%j.out
+#SBATCH -e params_lc_resamp/protoDC2_v0.4.5/logs/v0.4.5_z_0_1_%j.err
+
+
+
+./lc_resample.py params_lc_resamp/protoDC2_v0.4.5/protoDC2_v0.4.5_z_0_1.param
