b1480f35de049309474d15d557ce13f652de5ca3
Dan Korytov <dkorytov@gmail.com>
2018-09-07 04:42:15 +0000

    v1.0.0 param files

---
+++ params_lc_resamp/cosmoDC2_v1.0.0/run_cosmoDC2_v1.0.0_z_0_1_hp:a.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+#SBATCH --nodes=1
+#SBATCH --time=12:00:00
+#SBATCH --job-name=1.0.0_0_1
+#SBATCH -o params_lc_resamp/protoDC2_v1.0.0/logs/v1.0.0_z_0_1_%j.out
+#SBATCH -e params_lc_resamp/protoDC2_v1.0.0/logs/v1.0.0_z_0_1_%j.err
+
+
+
+./lc_resample.py params_lc_resamp/cosmoDC2_v1.0.0/cosmoDC2_v1.0.0_z_0_1_hp:a.param
---
+++ params_lc_resamp/cosmoDC2_v1.0.0/run_cosmoDC2_v1.0.0_z_2_3_hp:a.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+#SBATCH --nodes=1
+#SBATCH --time=12:00:00
+#SBATCH --job-name=1.0.0_2_3
+#SBATCH -o params_lc_resamp/protoDC2_v1.0.0/logs/v1.0.0_z_2_3_%j.out
+#SBATCH -e params_lc_resamp/protoDC2_v1.0.0/logs/v1.0.0_z_2_3_%j.err
+
+
+
+./lc_resample.py params_lc_resamp/cosmoDC2_v1.0.0/cosmoDC2_v1.0.0_z_2_3_hp:a.param
